1ba9dcc8e72103f9969edad686b15939fa5341b4
g14n4p3 <g14n4p3@g14n4p3s-MBP.fritz.box>
2021-07-29 10:13:18 +0200

    typos

--- src/views/Components/Sections/WhereToBuy.js
+++ src/views/Components/Sections/WhereToBuy.js
@@ -21,7 +21,7 @@ export default function WhereToBuy() {
             <div className={classes.title}>
                 <h2>How can i get IdaMurni</h2>
             </div>
-            <p>Actually you cannot buy IDA tokens yet, but you have opportunity to exchange again another exciting ERC20 tokens. there is a coupple places where you can get IDA tokens, visit decentralized exchange devethswap. swap any exiting token to IDA.</p>
+            <p>Actually you cannot buy IDA tokens yet, but you have opportunity to exchange agains another exciting ERC20 tokens. there is a coupple places where you can get IDA tokens, visit decentralized exchange devethswap. swap any exiting token to IDA.</p>
             <GridContainer>
                 <GridItem xs={12} sm={12} md={3} style={{textAlign: 'center'}}>
                 <a href="https://www.devethswap.de" target="_blank" style={{color: '#3d4a58'}}>
@@ -37,7 +37,7 @@ export default function WhereToBuy() {
                     <h3>IDA-Wallet</h3>
                 </div>
                 <img src={devethwallet} style={{marginBottom: 20, width: 100}}/>
-                <p className="text-white font-size-21 m-0">VISIT IDAHWALLET </p>
+                <p className="text-white font-size-21 m-0">VISIT IDAWALLET </p>
                 </GridItem>
             </GridContainer>
         </div>
